352dae75e03cb4deb3e18691e0ef205943b995bb
Jeffrey Walraven <jeffrey@ravenswall.net>
2019-10-03 00:00:45 -0400

    Switched to simpler demo test document

--- src/test/groovy/com/jtechdev/hateoasdocs/LinkRelationsDocumentSpec.groovy
+++ src/test/groovy/com/jtechdev/hateoasdocs/LinkRelationsDocumentSpec.groovy
@@ -7,28 +7,28 @@ import spock.lang.Specification
 
 class LinkRelationsDocumentSpec extends Specification {
 
-    def 'Converts iana yaml using object mapper'() {
+    def 'Converts demo01 yaml using object mapper'() {
         given: 'An object mapper'
         ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory()).registerModule(new KotlinModule())
 
-        when: 'Reading iana yaml file'
-        LinkRelationsDocument document = objectMapper.readValue(new File("src/test/resources/relation-docs/iana.yml"), LinkRelationsDocument)
+        when: 'Reading demo01 yaml file'
+        LinkRelationsDocument document = objectMapper.readValue(new File("src/test/resources/relation-docs/demo01.yml"), LinkRelationsDocument)
 
         then: 'The expected metadata matches'
-        document.title == 'IANA Link Relation Types'
-        document.description == 'The standard IANA link relations.'
-        document.reference == 'https://www.iana.org/assignments/link-relations/link-relations.xhtml'
-        document.formats == ['text', 'html', 'csv']
+        document.title == 'Demo01 Link Relation Types'
+        document.description == 'Just some demo link relation types.'
+        document.reference == 'http://somewebsite.com/demo01'
+        document.formats == ['text', 'html', 'csv', 'java']
 
         and: 'A fully described link relation matches'
-        with (document.linkRelations.find { it.name == 'about' }) {
+        with (document.linkRelations.find { it.name == 'custom-link' }) {
             description == 'Refers to a resource that is the subject of the link\'s context.'
-            reference == '[RFC6903], section 2'
-            notes == 'Used to link about pages.'
+            reference == '[RFC1149]'
+            notes == 'Used to link to custom pages.'
         }
 
         and: 'A minimally described link relation matches'
-        with (document.linkRelations.find { it.name == 'working-copy-of'}) {
+        with (document.linkRelations.find { it.name == 'other-custom-link'}) {
             !description
             !reference
             !notes
